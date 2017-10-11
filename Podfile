a780aefbd46f14905c743de8c35e0bd7d8636dc2
Tadeas Kriz <tadeas@brightify.org>
2017-10-11 15:00:29 +0200

    Initial commit.

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'SampleApp' do
+    
+  pod 'AFNetworking'
+end
+
+target 'SampleFramework' do
+  pod 'CocoaLumberjack'
+  pod 'Google/SignIn', '~> 3.0.3'
+end
+
---
+++ SampleFramework/TestViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TestViewController.swift
+//  SampleFramework
+//
+//  Created by Tadeas Kriz on 10/11/17.
+//  Copyright © 2017 benasher44. All rights reserved.
+//
+
+import UIKit
+import Google
+import GoogleSignIn
+
+public class TestViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    public override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
